02cd2c7df18a4b666ae4b9d5953774b1f4c9bb8e
manuelgu <manuelgu@users.noreply.github.com>
2015-11-05 11:15:09 +0100

    power led on remote changes

--- code/remote-led/script.py
+++ code/remote-led/script.py
@@ -6,12 +6,12 @@ import urllib2
 import time
 
 redPin = None
-bluePin = None
+yellowPin = None
 greenPin = None
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(redPin, GPIO.OUT)
-GPIO.setup(bluePin, GPIO.OUT)
+GPIO.setup(yellowPin, GPIO.OUT)
 GPIO.setup(greenPin, GPIO.OUT)
 
 def fetch():
@@ -23,20 +23,31 @@ def fetch():
 		if line == 'red':
 			# set color to red
 			print "Yay, red!"
-		elif line == 'blue':
+			empty()
+			GPIO.output(redPin, True)
+		elif line == 'yellow':
 			# set color to blue
-			print "Yay, blue!"
+			print "Yay, yellow!"
+			empty()
+			GPIO.output(yellowPin, True)
 		elif line == 'green':
 			# set color to green
 			print "Yay, green!"
+			empty()
+			GPIO.output(greenPin, True)
 		elif line == 'stop':
 			# turn all LEDs off
 			print "Aww, no colors!"
+			empty()
 		else:
 			print "Something went wrong.."
 
 	time.sleep(0.1)
 
+def empty():
+	GPIO.output(redPin, False)
+	GPIO.output(bluePin, False)
+	GPIO.output(greenPin, False)
 
 while True:
 	fetch()
